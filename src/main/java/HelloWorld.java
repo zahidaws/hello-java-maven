fccb62ba1238dc4b455e66b6bb08584234453ee3
zahid ahmed <xaidahm99@gmail.com>
2025-06-09 11:15:40 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,7 @@
-// HelloWorld.java public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, Jenkins +
- Maven!"); } }
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
+    }
+}
+
